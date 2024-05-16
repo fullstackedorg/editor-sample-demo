3f67e8acc638bb1d294657f06cca37d61fe4e7ec
cplepage <lepage.charlesphilippe@gmail.com>
2024-05-15 22:09:59 -0400

    classList is already an iterator

--- webview/icons.ts
+++ webview/icons.ts
@@ -1,5 +1,5 @@
 document.querySelectorAll(".icon").forEach(async (icon) => {
-    const iconName = Array.from(icon.classList.values())
+    const iconName = Array.from(icon.classList)
         .filter((iconClass) => iconClass !== "icon")
         .at(0);
     icon.innerHTML = await (
