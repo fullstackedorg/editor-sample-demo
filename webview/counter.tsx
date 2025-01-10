9393c22bc239bf4197c2578729a285bd727b08a2
cplepage <lepage.charlesphilippe@gmail.com>
2025-01-10 15:51:57 -0500

    0.10.0 fixes

--- webview/counter.tsx
+++ webview/counter.tsx
@@ -3,16 +3,7 @@ import { createRoot } from "react-dom/client";
 import fs from "fs";
 
 const countFile = "data/count.txt";
-const idFile = "data/id.txt";
 await fs.mkdir("data");
-
-let id: string;
-if (await fs.exists(idFile)) {
-    id = await fs.readFile(idFile, { encoding: "utf8" });
-} else {
-    id = Math.floor(Math.random() * 10000).toString();
-    await fs.writeFile(idFile, id);
-}
 
 function Icon(props: { iconName: string }) {
     const [icon, setIcon] = useState("");
@@ -28,68 +19,26 @@ function Icon(props: { iconName: string }) {
 
 async function loadCount() {
     if (!(await fs.exists(countFile))) return 0;
-
     return parseInt(await fs.readFile(countFile, { encoding: "utf8" }));
 }
 
-type CountCRDT = {
-    [id: string]: number;
-};
-
-// let throttler: ReturnType<typeof setTimeout> = null;
-// const sendCount = (count: number) => {
-//     if(throttler) clearTimeout(throttler);
-    
-//     throttler = setTimeout(() => {
-//         broadcast(JSON.stringify({ id, count }));
-//         throttler = null;
-//     }, 750);
-// };
-
 const initialCount = await loadCount();
 function Counter() {
-    const [counter, setCounter] = useState<CountCRDT>({
-        [id]: initialCount
-    });
-
-    // useEffect(() => {
-    //     sendCount(counter[id]);
-    // }, []);
+    const [count, setCount] = useState(initialCount);
 
     useEffect(() => {
-        // window.onPush["peerData"] = (message: string) => {
-        //     const peerCount = JSON.parse(message);
-
-        //     if (counter[peerCount.id] === undefined) {
-        //         sendCount(counter[id]);
-        //     }
-
-        //     counter[peerCount.id] = peerCount.count;
-        //     setCounter({ ...counter });
-        // };
-
-        fs.writeFile(countFile, counter[id].toString());
-    }, [counter]);
+        fs.writeFile(countFile, count.toString());
+    }, [count]);
 
     const decr = () => {
-        counter[id] -= 1;
-        setCounter({ ...counter });
-        sendCount(counter[id]);
+        setCount(count - 1);
     };
     const incr = () => {
-        counter[id] += 1;
-        setCounter({ ...counter });
-        sendCount(counter[id]);
+        setCount(count + 1);
     };
-    const reset = () => {
-        ipc
-            .fs.unlink(countFile)
-            .then(() => {
-                setCounter({
-                    [id]: 0
-                });
-                sendCount(0);
-            });
+    const reset = async () => {
+        await fs.unlink(countFile);
+        setCount(0);
     };
 
     return (
@@ -99,7 +48,7 @@ function Counter() {
                     <Icon iconName={"minus"} />
                 </button>
                 <div>
-                    {Object.values(counter).reduce((tot, count) => tot + count)}
+                    {count}
                 </div>
                 <button onClick={incr}>
                     <Icon iconName={"plus"} />
